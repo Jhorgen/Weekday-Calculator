7e7aa596ccb28f76af23e8c99c673e9814a6490d
Garland Lai and Joshua Horgen <garland@>
2019-06-27 09:54:37 -0700

    add 2nd spec to test another input

--- spec/Weekday-Calculator-spec.js
+++ spec/Weekday-Calculator-spec.js
@@ -6,4 +6,8 @@ describe( 'CalculateDay', function() {
     var userDate = new CalculateDay('6/29/2019');
     expect(userDate.returnResult()).toEqual('Saturday');
   });
+  it('should test whether the date input outputs the correct day', function() {
+    var userDate = new CalculateDay('6/27/2019');
+    expect(userDate.returnResult()).toEqual('Thursday');
+  });
 });
