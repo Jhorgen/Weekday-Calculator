2c81c256dca013c4d08da2197247623b379bddfb
Garland Lai <garlandlai24@gmail.com>
2019-06-26 19:18:01 -0700

    add some code to front end and backend

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+$(document).ready(function(){
+  $('#formOne').submit(function(){
+    event.preventDefault();
+    const month = $('#monthInput').val();
+    const day = $('#dayInput').val();
+    const year = $('#yearInput').val();
+    const complete = month + '/' + day + '/' + year
+    console.log(complete);
+  });
+});
